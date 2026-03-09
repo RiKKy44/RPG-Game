d37d10caea91392f01fdc34720faa772f48d805f
Kacper Frączek <kacper@tee.pl>
2026-03-09 12:40:08 +0100

    encoding fix

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿namespace OODProject;
+﻿using System.Text;
+
+namespace OODProject;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
         Game game = new Game();
         game.Run();
     }
