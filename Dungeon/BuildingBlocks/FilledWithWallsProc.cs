148198226762616175a4a26ae394725f1bc7fb56
Kacper Fraczek <kacperxl269@gmail.com>
2026-03-22 17:38:17 +0100

    implement Dungeon builder

---
+++ Dungeon/BuildingBlocks/FilledWithWallsProc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODProject.Dungeon.BuildingBlocks;
+
+public class FilledWithWallsProc : IBuildingBlock
+{
+    public void Apply(Board board)
+    {
+        for(int x = 0; x < GameConfig.Width; x++)
+        {
+            for(int y = 0; y < GameConfig.Height; y++)
+            {
+                board.SetField(new Position(x, y), new Wall());
+            }
+        }
+    }
+}
---
+++ Dungeon/DungeonBuilder.cs
@@ -0,0 +1,44 @@
+﻿using OODProject.Dungeon.BuildingBlocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODProject.Dungeon;
+
+
+public class DungeonBuilder
+{
+    private Board _board;
+
+    private bool _initialized = false;
+
+
+    public DungeonBuilder StartEmpty()
+    {
+        _board = new Board();
+        _initialized = true;
+        return this;
+    }
+
+    public DungeonBuilder StartFilled()
+    {
+        Apply(new FilledWithWallsProc());
+        _initialized = true;
+        return this;
+    }
+
+    public DungeonBuilder Apply(IBuildingBlock buildingBlock)
+    {
+        if (!_initialized)
+        {
+            throw new InvalidOperationException("Must call StartEmpty or StartFilled");
+        }
+
+        buildingBlock.Apply(_board);
+        return this;
+    }
+
+    public Board Build() => _board;
+}
