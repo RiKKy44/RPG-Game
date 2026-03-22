3387d71129999fccdeb85f462ae1593aaba07a57
Kacper Fraczek <kacperxl269@gmail.com>
2026-03-22 21:34:06 +0100

    implement chambers

---
+++ Dungeon/BuildingBlocks/AddChambersProc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODProject.Dungeon.BuildingBlocks;
+
+public class AddChambersProc
+{
+    private readonly int _count;
+
+    private readonly int _minSize;
+
+    private readonly int _maxSize;
+
+    private Random _random = new Random();
+
+    public AddChambersProc(int count = 2, int minSize = 3, int maxSize = 6)
+    {
+        _count = count;
+        _minSize = minSize;
+        _maxSize = maxSize;
+    }
+
+
+    public void Apply(Board board)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            int width = _random.Next(_minSize, _maxSize + 1);
+
+            int height = _random.Next(_minSize, _maxSize + 1);
+
+
+            //randomly choosing left upper corner
+            int x = _random.Next(1, GameConfig.Width - width - 1);
+
+            int y = _random.Next(1, GameConfig.Height - height - 1);
+
+
+            for (int dx = 0; dx < width; dx++)
+            {
+                for (int dy = 0; dy < height; dy++)
+                {
+                    board.SetField(new Position(x + dx, y + dy), new EmptyField());
+
+                }
+            }
+        }
+    }
+}
+
